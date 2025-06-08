6d93c67d9f4d5728c3aaad3dbc7fd8060a314aa8
Trasn0 <trasno@gmx.com>
2025-06-08 16:49:02 -0300

    Finaliza pipeline modularizada com DVC e MLflow

--- src/.ipynb_checkpoints/inference-checkpoint.py
+++ src/.ipynb_checkpoints/inference-checkpoint.py
@@ -5,6 +5,7 @@ from modulo import aula3_modulo_mlops as sts
 import mlflow
 import joblib
 
+mlflow.set_tracking_uri("http://127.0.0.1:5000") 
 
 #Dados Treino pata Inferencia
 seismic_slice_clean = np.load("outputs/seismic_slice_clean.npy")
@@ -19,6 +20,13 @@ sts.ET = ET
 seis_prop_vector, seis_estimated = sts.transfer_to_seismic_scale(dados_sismicos=seismic_slice_clean, modelo = ET, X=X, y=y)
 
 np.save("outputs/seis_estimated.npy", seis_estimated)
+
+with mlflow.start_run(run_name="inference"):
+    mlflow.log_params({"modelo": "ExtraTrees"})
+    mlflow.log_artifact("outputs/seis_estimated.npy")
+    print ("MLflow inferencia registrada")
+
+
 print ("Inferencia Pronta")
 
 
--- src/training.py
+++ src/training.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 from matplotlib.colors import TwoSlopeNorm
 from modulo import aula3_modulo_mlops as sts
 import mlflow
+import mlflow.sklearn
 import joblib
 
 sim_clean = np.load ("outputs/sim_clean.npy")
@@ -13,5 +14,15 @@ joblib.dump (ET, "outputs/model.pkl")
 
 np.save("outputs/X.npy",X)
 np.save("outputs/y.npy",y)
+
+mlflow.set_tracking_uri("http://127.0.0.1:5000")
+
+with mlflow.start_run(run_name="training"):
+    mlflow.log_params(dict_params)
+    mlflow.log_metric("NRMS",nrms_teste)
+    mlflow.log_metric("R2",r2_teste)
+    mlflow.log_metric("MAPE",mape_teste)
+    mlflow.sklearn.log_model(ET,"model")
+    print ("MLflow tracking realizado")
 
 print("Treino do ML pronto")
